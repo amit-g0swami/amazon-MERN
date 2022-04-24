3992791421def7b697d43016f00258a813a1e8e8
amit-g0swami <goswamiempire@gmail.com>
2022-04-24 07:41:26 +0530

    setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCO5BlXq1x1NbneJPKpb4SGMiTu0yun8PE",
+    authDomain: "clonemern-ddea3.firebaseapp.com",
+    projectId: "clonemern-ddea3",
+    storageBucket: "clonemern-ddea3.appspot.com",
+    messagingSenderId: "2825404638",
+    appId: "1:2825404638:web:c472c26c124a4e53bb024f",
+    measurementId: "G-05R6251HW3"
+};
